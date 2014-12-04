8c229a637c6b84d995132f7c0075b25067b34b2e
martind <martind@mapfactor.com>
2014-12-04 03:27:45 +0100

    simple conversion of scans to distance image

---
+++ log2pgm.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+"""
+  Convert log of laser scans into PGM image of distances
+    usage:
+         ./log2pgm.py <input log> <output image>
+"""
+
+import sys
+import math
+
+
+def loadAllScans( filename ):
+    arr = []
+    for line in open(filename):
+        arr.append( eval(line) )
+    return arr
+
+def dist2gray( value ):
+    return min(255, value/5)
+
+
+def log2pgm( filename, outputFilename ):
+    f = open( outputFilename, "w" )
+    scans = loadAllScans( filename )
+    f.write( "P2\n%d %d\n255\n" % (len(scans), len(scans[0])) )
+    for y in xrange( len(scans[0]) ):
+        for x in xrange(len(scans)):
+            f.write( "%d " % dist2gray(scans[x][y]) )
+        f.write( "\n" )
+    f.close()
+
+
+if __name__ == "__main__": 
+    if len(sys.argv) < 3:
+        print __doc__
+        sys.exit(1)
+    log2pgm( sys.argv[1], sys.argv[2] )
+
+# vim: expandtab sw=4 ts=4
+
