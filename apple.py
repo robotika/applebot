003b0f5e0b31b5519409f0110422afde3d4defb3
martind <martind@mapfactor.com>
2014-12-06 17:06:55 +0100

    some real examples + implementation of saveAsCloud

--- apple.py
+++ apple.py
@@ -7,13 +7,33 @@ import math
 import cv2
 import numpy as np
 
+from log2pts import MOTION_STEP_X
+
 class Apple:
     def __init__( self, patch ):
-        w,h = patch.shape
-        inside = patch[w/4:3*w/4,h/4:3*h/4]
-        print np.amax(inside) - np.amin(inside)
-        print inside
+        self.data = patch.copy()
+
 
     def fitSphere( self ):
-        return 0.5 # maybe yes, maybe not
+        # TODO
+        return 0.5 # maybe yes, maybe no
+
+
+    def points( self ):
+        ret = []
+        x = 0.0
+        for arr in self.data.T.tolist():
+            for i, a in enumerate(arr):
+                angle = math.radians(i-len(arr)/2)
+                dist = a/1000.0
+                ret.append( (x, dist*math.cos(angle), dist*math.sin(angle)) )
+            x += MOTION_STEP_X 
+        return ret
+
+
+    def saveAsCloud( self, filename ):
+        f = open( filename, "w" )
+        for x,y,z in self.points():
+            f.write( "%.3f %.3f %.3f\n" % (x, y, z) )
+        f.close()
 
