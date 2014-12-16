c0af5002bb2a2afc918442afa4286d4c8f2a0403
martind <martind@mapfactor.com>
2014-12-16 21:37:41 +0100

    collecting reminission and synchronous camera pictures

--- scan3d.py
+++ scan3d.py
@@ -13,6 +13,18 @@ import datetime
 sys.path.append( ".."+os.sep+"eduro") 
 
 import laser
+import urllib
+
+URL = "http://192.168.1.6/img.jpg"
+
+def takePicture( index ):
+    filename = datetime.datetime.now().strftime("logs/pic_%y%m%d_%H%M%S") + "_%03d.jpg" % index
+    url = urllib.urlopen( URL ) 
+    f = open( filename, "wb" )
+    data = url.read(1000000)
+    print len(data)
+    f.write( data )
+    f.close()
 
 
 def scan( laser, num ):
@@ -21,8 +33,10 @@ def scan( laser, num ):
     f = open( filename, "w" )
     laser.startLaser()
     for i in xrange(num):
+        if i % 10 == 0:
+            takePicture( i/10 )
         data, reminision = laser.internalScan()
-        f.write( str(data) + '\n' )
+        f.write( str( (data, reminision) ) + '\n' )
         f.flush()
         print len(data), data[133:138]
     f.close()
@@ -31,7 +45,8 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print __doc__
         sys.exit(1)
-    laser.HOST, laser.PORT = '169.254.225.156', 2111
+#    laser.HOST, laser.PORT = '169.254.225.156', 2111
+    laser.HOST, laser.PORT = '192.168.1.156', 2111
     print datetime.datetime.now()
     print scan( laser.LaserIP(), num=int(sys.argv[1]) )
     print datetime.datetime.now()
