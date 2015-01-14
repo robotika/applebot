859b1fb49271b2dadfd884d5d3b7199c61399ccd
martind <martind@mapfactor.com>
2015-01-14 20:42:39 +0100

    first experiments with real UR5

--- ur5.py
+++ ur5.py
@@ -10,8 +10,9 @@
 import sys
 import time
 import socket
+import math
 
-UR5_HOST = "192.168.1.18" # TODO 
+UR5_HOST = "192.168.1.51" # TODO 
 UR5_PORT = 30002
 
 
@@ -25,8 +26,8 @@ class UniversalRobotUR5:
     def __init__( self ):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.connect((UR5_HOST, UR5_PORT))
-        self.acc = 1.3962634015954636
-        self.speed = 1.0471975511965976
+        self.acc = 0.13962634015954636
+        self.speed = 0.10471975511965976
 
     def term( self ):
         self.s.close()
@@ -43,15 +44,24 @@ class UniversalRobotUR5:
         pass
 
     def testIO( self ):
-        self.s.send("set_digital_out(2,True)" + "\n")
+#        self.s.send("set_digital_out(8,True)" + "\n") # tool 0
+        self.s.send("set_digital_out(8,False)" + "\n") # tool 0
         data = self.s.recv(1024)
         print "Received", repr(data)
 
 
 def testUR5( cmd ):
     robot = UniversalRobotUR5()
-    robot.testIO()
+#    robot.testIO()
 #    robot.movej( [-0.5405182705025187, -2.350330184112267, -1.316631037266588, -2.2775736604458237, 3.3528323423665642, -1.2291967454894914] )
+#    robot.movej( [math.radians(x) for x in [0, -90, 0, -90, 0, 0]] ) # straight position
+    robot.s.send("movej( p[0.2, 0.2, 0.6, 0, -2.2218, 2.2228], a=0.1, v=0.1 )\n") # p - cartesian coordinates :)
+
+# display coordinates
+#    robot.s.send('popup("hi")\n')
+#    robot.s.send("def myProg():\nm = get_inverse_kin( get_target_tcp_pose())\npopup( m )\nend\nmyProg()\n")
+#    robot.s.send("def myProg():\nm = get_target_tcp_pose()\npopup( m )\nend\nmyProg()\n")
+
     robot.term()
 
 
